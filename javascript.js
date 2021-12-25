e4a16451b81ea178b4dba002c94d3fdc6b1ee60a
YeoInsu <insu12021202@gmail.com>
2021-12-25 19:54:09 +0900

    add action btn to toggle

--- javascript.js
+++ javascript.js
@@ -1,4 +1,10 @@
 const channelBtn = document.getElementById("channelBtn");
+const actionBtn1 = document.getElementById("action1");
+const actionBtn2 = document.getElementById("action2");
+const actionBtn3 = document.getElementById("action3");
+const actionBtn4 = document.getElementById("action4");
+const actionBtn5 = document.getElementById("action5");
+const background = document.getElementsByClassName("background");
 
 function handleChannelBtn(){
     if (channelBtn.classList.contains("clickedSubscribe")){
@@ -10,6 +16,19 @@ function handleChannelBtn(){
         channelBtn.classList.add("clickedSubscribe");
         channelBtn.style.color = "#ff0000";
     }
-    
 }
+
+function handleActionBtn(event){
+    event.target.classList.toggle("actionClicked");
+}
+
+function handlePopUp(){
+    background.classList.toggle("show");
+}
+
+actionBtn1.addEventListener("click", handleActionBtn);
+actionBtn2.addEventListener("click", handleActionBtn);
+actionBtn3.addEventListener("click", handleActionBtn);
+actionBtn5.addEventListener("click", handleActionBtn);
 channelBtn.addEventListener("click", handleChannelBtn);
+actionBtn4.addEventListener("click", handlePopUp);
